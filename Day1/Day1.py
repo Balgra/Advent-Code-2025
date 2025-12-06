ea8d32fddc20a3255491e16765ac4df698b6236c
Balgra <andreibalgradean16@gmail.com>
2025-12-06 15:44:32 +0100

    Solution Day1 Part 2

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,26 +1,53 @@
+
+
 import file_reader
 
 input_list=file_reader.input_reader()
+
+#Part1
+
+# for dial_move in input_list:
+#     if 'L' in dial_move:
+
+#         dial =(dial - int(dial_move.lstrip("L")) ) % 100
+
+#     if 'R' in dial_move:
+
+#         dial =(dial + int(dial_move.lstrip("R")) )% 100
+
+#     if dial == 0:
+#         count+=1
+
+# print(count)
+
+
+
+#Part 2
 
 dial = 50
 count=0
 
 for dial_move in input_list:
-    if 'L' in dial_move:
 
-        dial =(dial - int(dial_move.lstrip("L")) ) % 100
+    if 'L' in dial_move:
+        dial_comp = dial - int(dial_move.lstrip("L"))
+
+        if dial_comp < 1:
+            count += ((dial-1) // 100) - ((dial_comp-1) // 100)  #trick floor function (parition coverage start 50 L100 and so on)
+
+        dial =(dial_comp) % 100
 
     if 'R' in dial_move:
+        dial_comp = dial + int(dial_move.lstrip("R"))
 
-        dial =(dial + int(dial_move.lstrip("R")) )% 100
+        if dial_comp>99:
+            count+= dial_comp//100
 
-    if dial == 0:
-        count+=1
+        dial =dial_comp % 100
+
 
 print(count)
 
-# Which makes sense since we can have input as more than 3 digits, no matter the way we
-# are going we require to use modulo, as we need to stay between 0 and 99.
-# And for values < 100 modulo does nothing. Meanwhile it helps for values >= 100
+# That's the right answer! You are one gold star closer to decorating the North Pole.
 
-#That's the right answer! You are one gold star closer to decorating the North Pole.
+# You have completed Day 1!
