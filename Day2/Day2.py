e5ed49a7584589152dc7c134d8373a919f2d2ad6
Balgra <andreibalgradean16@gmail.com>
2025-12-07 00:35:28 +0100

    Day2 Part2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,33 +1,67 @@
 import file_reader
-import logging
+# import logging
 
-# logging.basicConfig(filename='output.log', level=logging.INFO)
+# # logging.basicConfig(filename='output.log', level=logging.INFO)
 
 input_dict=file_reader.input_reader()
-# print(input_dict)
-# only of some sequence of digits repeated twice
+# # print(input_dict)
+# # only of some sequence of digits repeated twice
+
+# result = 0
+
+# def check_duplicate(start,finish):
+#     result=0
+#     for i in range(start,finish+1):
+#         i=str(i)
+#         if len(i) %2 ==0:
+#             l=0
+#             r=len(i)//2
+#             rez=0
+#             while l<len(i)//2 and r<len(i):
+#                 if i[l] == i[r]:
+#                     rez+=1
+#                 l+=1
+#                 r+=1
+#             if rez == len(i)//2:
+#                 # logging.info(i)
+#                 result+=int(i)
+#             else:
+#                 result+=0
+#     return result
+        
+
+# for key,value in input_dict.items():
+
+#     result+=check_duplicate(key,value)
+
+# print(result)
+
+# That's the right answer! You are one gold star closer to decorating the North Pole. [Continue to Part Two]
+
+
+#Part2
 
 result = 0
 
-def check_duplicate(start,finish):
-    result=0
-    for i in range(start,finish+1):
-        i=str(i)
-        if len(i) %2 ==0:
-            l=0
-            r=len(i)//2
-            rez=0
-            while l<len(i)//2 and r<len(i):
-                if i[l] == i[r]:
-                    rez+=1
-                l+=1
-                r+=1
-            if rez == len(i)//2:
-                # logging.info(i)
-                result+=int(i)
-            else:
-                result+=0
-    return result
+def is_repetition(num):
+    s = str(num)
+    n = len(s)
+    
+    for size in range(1, n//2 + 1):
+        if n % size == 0: 
+            chunk = s[:size] # 824824824 -> 8, 82, 824
+            if chunk * (n // size) == s:
+                return True  
+                
+    return False  
+
+
+def check_duplicate(start, finish):
+    rez = 0
+    for i in range(start, finish + 1):
+        if is_repetition(i):
+            rez += i
+    return rez
         
 
 for key,value in input_dict.items():
@@ -36,4 +70,4 @@ for key,value in input_dict.items():
 
 print(result)
 
-# That's the right answer! You are one gold star closer to decorating the North Pole. [Continue to Part Two]
+# Both parts of this puzzle are complete! They provide two gold stars: **
