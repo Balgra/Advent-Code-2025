beb0c88e8d1939ab800ea7c8339179574fbcc0bc
Balgra <andreibalgradean16@gmail.com>
2025-12-26 16:48:41 +0100

    Day 11 part 2

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,18 +1,44 @@
 
 import file_reader
+from functools import cache
 
 
 lines = file_reader.input_reader()
 
 dict_nodes={}
 
+# def paths(start: str, end: str) -> int:
+
+#     if start == end :
+#         return 1
+    
+#     return sum(paths(n, end) for n in dict_nodes[start])
+
+
+
+# for line in lines:
+
+#     node, *outs = line.split()
+
+#     dict_nodes[node.strip(':')] = outs
+
+# # print(dict_nodes)
+
+# rez = paths('you', 'out')
+
+# print(rez)
+
+
+# That's the right answer! You are one gold star closer to decorating the North Pole.
+
+
+@cache
 def paths(start: str, end: str) -> int:
 
     if start == end :
         return 1
     
-    return sum(paths(n, end) for n in dict_nodes[start])
-
+    return sum(paths(n, end) for n in dict_nodes.get(start, []))
 
 
 for line in lines:
@@ -22,11 +48,21 @@ for line in lines:
     dict_nodes[node.strip(':')] = outs
 
 # print(dict_nodes)
+#check for whether fft or dac first:
 
-rez = paths('you', 'out')
+if paths('fft', 'dac'):
+    
+    first, second = ('fft', 'dac')
+else:
+   first, second = ('dac', 'fft')
+
+
+
+rez = paths('svr', first) * paths(first, second) * paths(second, 'out')
 
 print(rez)
 
 
 # That's the right answer! You are one gold star closer to decorating the North Pole.
 
+# You have completed Day 11!
